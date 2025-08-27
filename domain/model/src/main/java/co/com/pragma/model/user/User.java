0be6c78ada8eaecef3230a0503fa268745539778
Edmh <edavidmanotas@gmail.com>
2025-08-27 06:47:59 -0500

    feat(domain): add User entity and UserRepository port

---
+++ domain/model/src/main/java/co/com/pragma/model/user/User.java
@@ -0,0 +1,23 @@
+package co.com.pragma.model.user;
+import lombok.*;
+//import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder(toBuilder = true)
+public class User {
+
+    private Long id;
+    private String name;
+    private String lastName;
+    private LocalDate birthDate;
+    private String address;
+    private String phone;
+    private String email;
+    private Double baseSalary;
+
+}
